7b381b89f40277a38a84e5494202aede4f24169e
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-30 11:32:33 +0200

    Add the model User and the validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
+  has_many :reservations
+  has_many :cars, through: :reservations
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 30 }
 end
