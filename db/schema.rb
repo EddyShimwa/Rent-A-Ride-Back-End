38e0af4ddf5c890b8513c47dbf7d071c9e8e9202
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 07:19:55 +0000

    add route to register a user

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_070656) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "name"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
