de78e7c060200a69a004627db4c6739cb63c1c57
EddyShimwa <shedpray16@gmail.com>
2023-05-30 14:03:11 +0200

    modified the car model attributes

--- db/migrate/20230530084623_create_cars.rb
+++ db/migrate/20230530084623_create_cars.rb
@@ -4,7 +4,7 @@ class CreateCars < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :model
       t.string :description
-      t.integer :integer
+      t.integer :rating
       t.decimal :price, precision: 10, scale: 2
       t.integer :rent_per_day
       t.references :user, null: false, foreign_key: true
