f90b83e26e0b69eb632dcc990505d4e8c00f7d5d
EddyShimwa <shedpray16@gmail.com>
2023-05-30 14:06:21 +0200

    added the initial data in seeds file
    Co-authored-by: kennedy <kennediowusu@gmail.com>
    Co-authored-by: Pierre <piercel.coder2022@gmail.com>

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :users, through: :rentals
 
-  validates :name, length: { maximum: 30 } 
+  validates :name, length: { maximum: 30 }
   validates :description, length: { maximum: 2000 }
   validates :rent_per_day, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,8 @@ admin = User.create(
 ## Cars
 car1 = Car.create(name: "toyota", model: "v8", description: "the most confort car", rating: 2, price: 9.99, rent_per_day: 4, user_id: admin.id)
 
-car1 = Car.create(name: "Benz Mercedes", model: "G-class", description: "the 
-        most confort car", rating: 2, price: 7.99, rent_per_day: 4, user_id: admin.id)
+car1 = Car.create(name: "Benz Mercedes", model: "G-class", description: "the most confort car", rating: 2, price: 7.99, rent_per_day: 4, user_id: admin.id)
     
-        #Rentals 
+#Rentals 
 firstRental = Rental.create(start_date: Date.today, end_date: Date.today + 7.days, city: "Lagos", price_per_day: 754, car_id: car1.id, user_id: admin.id)
             
