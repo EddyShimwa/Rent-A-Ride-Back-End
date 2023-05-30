dc25c3b5f4a3ccf0775c4d56a7e02c9dc209dd6f
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 07:17:59 +0000

    add user controller for creating users with action for user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password)
+  end
+end
