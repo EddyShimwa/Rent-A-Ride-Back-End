8ebb564569ef4adef7ccdf286c88ac220bb9e997
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 07:17:25 +0000

    update the schema and the user migration table

--- db/migrate/20230530070656_create_users.rb
+++ db/migrate/20230530070656_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :name
+      t.string :email, unique: true
       t.string :password_digest
 
       t.timestamps
