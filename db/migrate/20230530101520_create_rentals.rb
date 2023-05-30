0a373761b4c7f518d24e9a0f13f474a362704546
EddyShimwa <shedpray16@gmail.com>
2023-05-30 12:19:13 +0200

    add the migration for rentals

---
+++ db/migrate/20230530101520_create_rentals.rb
@@ -0,0 +1,14 @@
+class CreateRentals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rentals do |t|
+      t.date :start_date
+      t.date :end_date
+      t.string :city
+      t.string :price_per_day
+      t.references :car, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
