10aaffbbe00534b477db1a764bae708ea8ccd16b
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 07:45:27 +0000

    add method for logging user in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :accepted
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
   private
 
   def user_params
