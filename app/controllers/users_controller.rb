4f9182f6a0a664b55335a5188bbe4d1a7ee3e714
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 07:35:53 +0000

    add name field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :name, :password)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
