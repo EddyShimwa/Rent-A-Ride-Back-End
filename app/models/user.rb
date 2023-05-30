b094dcb155462233bbc73df6b086b76d877ff6c1
EddyShimwa <shedpray16@gmail.com>
2023-05-30 13:03:27 +0200

    modified the user model
    Co-authored-by: kennedy <kennediowusu@gmail.com>
    Co-authored-by: Pierre <piercel.coder2022@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 30 }
 end
