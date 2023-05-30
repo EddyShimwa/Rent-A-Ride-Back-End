74049354225ce27eddfe5b4b59f577b27fd7f601
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 07:36:20 +0000

    remove name column

--- db/migrate/20230530070656_create_users.rb
+++ db/migrate/20230530070656_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
       t.string :email, unique: true
       t.string :password_digest
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_070656) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_072819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_070656) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
