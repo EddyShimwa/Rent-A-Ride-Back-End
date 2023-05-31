f3b30aabe1b38164fa7ea76e2d84fb39199acfdf
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-31 22:44:59 +0200

    Validate the car model

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,3 +1,44 @@
 require 'rails_helper'
 
 ## testing car model
+RSpec.describe Car, type: :model do
+    let(:user) { create(:user) }
+    let(:car) do
+      Car.create(
+        name: 'MERCEDES BENZ G CLASS',
+        description: 'MERCEDES G63 AMG - SERIES 21: S21-02',
+        rent_per_day: '210',
+        user:
+      )
+    end
+  
+    it 'car should be valid with a name' do
+      car.name = 'MERCEDES BENZ G CLASS'
+      expect(car).to be_valid
+    end
+  
+    it 'is not valid with a description smaller than 1000' do
+      car.description = 'Contrary to popular belief, Lorem Ipsum is not simply random text.
+      It has roots in a piece of classical Latin literature from 45 BC, making it over 2000
+      years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
+      looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going
+      through the cites of the word in classical literature, discovered the undoubtable source.
+      Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+      Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of
+      ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+      amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s
+      is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+      Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions
+      from the 1914 translation by H. Rackham.'
+      expect(car).to be_valid
+    end
+
+    it 'is not valid without a price' do
+      car.rent_per_day = nil
+      expect(car).to_not be_valid
+    end
+    it 'is not valid without a price greater than 0' do
+      car.rent_per_day = -50
+      expect(car).to_not be_valid
+    end
+  end
