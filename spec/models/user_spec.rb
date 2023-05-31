408a940cf815b5c4c4c80b10486456befec88b45
EddyShimwa <shedpray16@gmail.com>
2023-05-31 20:31:20 +0200

    add more specs examples for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe User, type: :model do
   it 'Should be valid ' do
     expect(subject).to be_valid
   end
+  
+  it 'Name should not be nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
+  it 'car should have a name with length no less that 3' do
+    subject.name = 'rl'
+    expect(subject).to_not be_valid
+  end
 
 end
