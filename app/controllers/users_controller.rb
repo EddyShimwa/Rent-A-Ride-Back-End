a4f91df1ffb81e2efff400e6696ed88dd992dfb5
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-31 09:29:46 +0000

    add index and show methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def create
     @user = User.create(user_params)
     if @user.valid?
