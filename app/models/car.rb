3f2413ea087ced3c9a7c9cec79625a7b8b6873e9
EddyShimwa <shedpray16@gmail.com>
2023-06-05 17:43:53 +0200

    add the validations to the image column

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ApplicationRecord
   has_many :users, through: :rentals
 
   validates :name, length: { maximum: 30 }, presence: true
+  validates :car_image_url, presence: true
   validates :description, presence: true, length: { maximum: 2000 }
   validates :rent_per_day, numericality: { greater_than_or_equal_to: 0 }
 end
