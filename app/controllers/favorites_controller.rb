23d33c3e7aac647f670b83f63a6def57096222d8
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-06 08:27:56 +0000

    create favorite controller

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,33 @@
+class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.all
+    render json: @favorites
+  end
+
+  def show
+    @favorite = Favorite.find(params[:id])
+    render json: @favorite
+  end
+
+  def create
+    @favorite = Favorite.create(favorite_params)
+    if @favorite.valid?
+      render json: @favorite, status: :created
+    else
+      render json: { error: 'failed to create favorite' }, status: :not_acceptable
+    end
+
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    render json: 'Favorite deleted successfully'
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:user_id, :car_id, :name, :description, :image)
+  end
+end
